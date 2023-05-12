e3415288ca48cda09db34297766f8474d982b3f3
Anastas Acevski <anastas.acevski@outlook.com>
2023-05-12 11:39:36 +0200

    Thymeleaf Templates

--- src/main/java/springframework/spring6webapp/controllers/BookController.java
+++ src/main/java/springframework/spring6webapp/controllers/BookController.java
@@ -8,10 +8,10 @@ import springframework.spring6webapp.services.BookService;
 @Controller
 public class BookController {
     private final BookService bookService;
+
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-
     @RequestMapping("/books")
     public String getBooks(Model model) {
 
